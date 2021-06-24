b625440391ef6c91e4882462df56783ee9e1a94c
Sagar Dhandare <sagardhandare3@gmail.com>
2021-06-24 08:50:29 +0530

    Add files via upload

---
+++ Liver.py
@@ -0,0 +1,36 @@
+# Importing Libraries:
+import pandas as pd
+import numpy as np
+import pickle
+
+# for displaying all feature from dataset:
+pd.pandas.set_option('display.max_columns', None)
+
+# Reading Dataset:
+dataset = pd.read_csv("Liver_data.csv")
+
+# Filling NaN Values of "Albumin_and_Globulin_Ratio" feature with Median:
+dataset['Albumin_and_Globulin_Ratio'] = dataset['Albumin_and_Globulin_Ratio'].fillna(dataset['Albumin_and_Globulin_Ratio'].median())
+
+# Label Encoding:
+dataset['Gender'] = np.where(dataset['Gender']=='Male', 1,0)
+
+# Droping 'Direct_Bilirubin' feature:
+dataset = dataset.drop('Direct_Bilirubin', axis=1)
+
+# Independent and Dependent Feature:
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
+
+# Train Test Split:
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size=0.3, random_state=33)
+
+# RandomForestClassifier:
+from sklearn.ensemble import RandomForestClassifier
+RandomForest = RandomForestClassifier()
+RandomForest = RandomForest.fit(X_train,y_train)
+
+# Creating a pickle file for the classifier
+filename = 'Liver.pkl'
+pickle.dump(RandomForest, open(filename, 'wb'))
